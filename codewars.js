62c6a9322d654cb193ed8234ba8a13dc27c56abe
relero90 <relero90@gmail.com>
2022-10-20 11:32:42 -0600

    validatePIN algo

--- codewars.js
+++ codewars.js
@@ -1,7 +1,11 @@
-// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
-const int1 = -3;
-const int2 = 0;
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+// If the function is passed a valid PIN string, return true, else return false.
+function validatePIN(pin) {
+  let regex = /^([\d]{4}|[\d]{6})$/;
+  return regex.test(pin.toString());
+}
 
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 function getSum(a, b) {
   let sum = 0;
   let difference = Math.abs(a - b);
