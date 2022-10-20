cc4deae4395511df0717c99f6150cae30199e0be
relero90 <relero90@gmail.com>
2022-10-20 11:10:04 -0600

    getSum algo

--- codewars.js
+++ codewars.js
@@ -1,3 +1,20 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+const int1 = -3;
+const int2 = 0;
+
+function getSum(a, b) {
+  let sum = 0;
+  let difference = Math.abs(a - b);
+  let startingAddend;
+  a < b ? (startingAddend = a) : (startingAddend = b);
+  while (difference >= 0) {
+    sum += startingAddend;
+    startingAddend = startingAddend + 1;
+    difference = difference - 1;
+  }
+  return sum;
+}
+
 // Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
 // Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
 // If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
