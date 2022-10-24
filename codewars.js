f20e2018e7799b896b7c48b7e0fcc4d29ae852c0
relero90 <relero90@gmail.com>
2022-10-24 17:55:11 -0600

    humanReadable

--- codewars.js
+++ codewars.js
@@ -35,7 +35,6 @@ function humanReadable(seconds) {
 
   return `${hours}:${minutes}:${secs}`;
 }
-console.log(humanReadable(7820)); // expect '00:59:59'
 
 // Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
 function duplicateCount(text) {
