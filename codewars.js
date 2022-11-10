ec35636976dfdd8ab1ab642b795aa944107d49e9
relero90 <relero90@gmail.com>
2022-11-10 13:21:50 -0700

    function likes algo

--- codewars.js
+++ codewars.js
@@ -1,3 +1,30 @@
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return `no one likes this`;
+      break;
+    case 1:
+      return `${names[0]} likes this`;
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]}, and ${names[2]} like this`;
+      break;
+    default:
+      return `${names[0]} and ${names[1]} and ${
+        names.length - 2
+      } others like this`;
+      break;
+  }
+}
+
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 function squareSum(numbers) {
   let sum = 0;
