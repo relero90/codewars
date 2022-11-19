126bcadd282781a5a68171c76076aa216d27d1af
relero90 <relero90@gmail.com>
2022-11-19 15:12:09 -0700

    alphanumeric algo solved with regex.test()

--- codewars.js
+++ codewars.js
@@ -4,8 +4,13 @@
 // Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
 // No whitespaces / underscore
 function alphanumeric(string) {
-  //your code here
+  let regex = /^[\da-zA-Z]+$/;
+  return regex.test(string);
 }
+// console.log(alphanumeric("Mazinkaiser")); //expect true
+// console.log(alphanumeric("hello world_")); //expect false
+// console.log(alphanumeric("PassW0rd")); //expect true
+// console.log(alphanumeric("     ")); //expect false
 
 // You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
 // Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
